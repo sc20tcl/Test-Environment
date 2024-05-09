4535276abea09015644963b0c07a0eb34bf8fb3d
sc20tcl <162443203+sc20tcl@users.noreply.github.com>
2024-05-09 01:30:39 +0100

    Update podTestOrchestrator.py

--- podTestOrchestrator.py
+++ podTestOrchestrator.py
@@ -116,7 +116,7 @@ def run_test(filepath, replica_array):
 with open("directory-test.txt", 'w') as file:
             file.write("This is a test file created by Python.\n")
 
-replica_array = [5]
+replica_array = [9]
 
 for replicas in replica_array:
     print("replicas: ", replicas)
