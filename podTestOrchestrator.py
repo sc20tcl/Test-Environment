a2e24b545e268d1c210ded52a901b4030351bea8
sc20tcl <162443203+sc20tcl@users.noreply.github.com>
2024-05-09 18:58:34 +0100

    Update podTestOrchestrator.py

--- podTestOrchestrator.py
+++ podTestOrchestrator.py
@@ -123,7 +123,7 @@ for replicas in replica_array:
     print("replicas: ", replicas)
     scale_deployment("teastore-webui", replicas)
     print("5 minute cool down...")
-    time.sleep(300)
+    # time.sleep(300)
     file_path = f'pod_model_results3_{replicas}.csv'
     run_test(file_path, replica_array) 
 
