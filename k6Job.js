2f127743b68adf5acf0fb90402c0fa5f16135477
sc20tcl <162443203+sc20tcl@users.noreply.github.com>
2024-05-10 20:07:27 +0100

    Update k6Job.js
    updated urls after cluster restart

--- k6Job.js
+++ k6Job.js
@@ -20,11 +20,11 @@ export const options = {
 
 
 const urls = [
-    'http://4.158.24.106:8080/tools.descartes.teastore.webui/category?category=2&page=1',
-    'http://4.158.24.106:8080/tools.descartes.teastore.webui/category?category=3&page=1',
-    'http://4.158.24.106:8080/tools.descartes.teastore.webui/category?category=4&page=1',
-    'http://4.158.24.106:8080/tools.descartes.teastore.webui/product?id=207',
-    'http://4.158.24.106:8080/tools.descartes.teastore.webui/category?category=6&page=1'
+    'http://4.158.172.95:8080/tools.descartes.teastore.webui/category?category=2&page=1',
+    'http://4.158.172.95:8080/tools.descartes.teastore.webui/category?category=3&page=1',
+    'http://4.158.172.95:8080/tools.descartes.teastore.webui/category?category=4&page=1',
+    'http:/4.158.172.95:8080/tools.descartes.teastore.webui/product?id=207',
+    'http://4.158.172.95:8080/tools.descartes.teastore.webui/category?category=6&page=1'
 ];
 
 export default function () {
